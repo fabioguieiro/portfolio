c5fad63c3a7282fb064ac4b9501e696c84dcec61
fabioguieiro <fabioguieiro@gmail.com>
2024-09-06 16:50:25 -0300

    feat: banner adjusts

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -5,9 +5,9 @@ export const Banner = () => {
   const t = useTranslations("HomePage");
 
   return (
-    <div className="w-full py-10  h-auto md:h-80 bg-petroleum flex flex-col md:flex-row items-center justify-center md:justify-evenly border-b-4 border-black dark:border-gold dark:bg-brownie">
+    <div className="w-full py-10  h-auto md:h-full bg-tomato flex flex-col md:flex-row items-center justify-center md:justify-center border-b-4 border-black dark:border-gold dark:bg-brownie">
       <Avatar />
-      <div className="w-full mt-4 md:mt-0 md:w-1/4">
+      <div className="w-full mt-4 md:mt-0 md:w-1/4 md:ml-10">
         <h1 className="font-hand text-center md:text-left  text-3xl text-cream mb-4">
           {t("bannerHeadline")}
         </h1>
