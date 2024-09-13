2362e3ae1511f18d952295487c1e16a522bab8dd
fabioguieiro <fabioguieiro@gmail.com>
2024-09-13 12:12:52 -0300

    fix on inter image asset

--- public/images/Money.jsx
+++ public/images/Money.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Money({ width = 240, height = 240 }) {
   return (
     <Image
-      src={`/Money.png`}
+      src={`/money.png`}
       alt={"illustration of a pack of bills running"}
       width={width}
       height={height}
